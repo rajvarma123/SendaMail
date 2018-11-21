9694b4ffe782583d8598751aea5e801b98ab8635
raj <rajvarma007009@gmail.com>
2018-11-21 15:21:52 +0530

    Update Program.cs

--- SendaMail/Program.cs
+++ SendaMail/Program.cs
@@ -4,6 +4,14 @@ using System.Net.Mail;
 
 namespace EmailSmtp
 {
+    public class SendMail
+    {
+        public string Subject;
+        public string Body;
+        public string From;
+        public string To;
+        public string Password;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -11,17 +19,30 @@ namespace EmailSmtp
             try
             {
                 // Credentials
-                var credentials = new NetworkCredential("testexample@gmail.com", "password");
+                
+                SendMail ss = new SendMail();
+                Console.WriteLine("enter your gmail");
+                ss.From = Console.ReadLine();
+                Console.WriteLine("enter your Password");
+                ss.Password = Console.ReadLine();
 
+                Console.WriteLine("enter the subject");
+                ss.Subject = Console.ReadLine();
+                Console.WriteLine("enter the body");
+                ss.Body = Console.ReadLine();
+                Console.WriteLine("Enter for to address");
+                ss.To = Console.ReadLine();
+                var credentials = new NetworkCredential(ss.From, ss.Password);
                 // Mail message
                 var mail = new MailMessage()
                 {
-                    From = new MailAddress("testexample@gmail.com"),
-                    Subject = "Test email.",
-                    Body = "Test email body"
+
+                    From = new MailAddress(ss.From),
+                    Subject = ss.Subject,
+                    Body = ss.Body,
                 };
 
-                mail.To.Add(new MailAddress("amlakings@gmail.com"));
+                mail.To.Add(new MailAddress(ss.To));
 
                 // Smtp client
                 var client = new SmtpClient()
